ab9723c0506f7f0c75230fa573e6f2a8a9a2d98f
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2025-01-14 11:33:17 +0530

    Refactor sensor checks to use direct variable access instead of digitalRead

--- code/checkMaze.cpp
+++ code/checkMaze.cpp
@@ -4,17 +4,17 @@
 bool wallLeft()
 {
   // Check the left sensor
-  return digitalRead(sensor_left) < side_threshold;
+  return sensor_left < side_threshold;
 }
 
 bool wallRight()
 {
   // Check the right sensor
-  return digitalRead(sensor_right) < side_threshold;
+  return sensor_right < side_threshold;
 }
 
 bool wallFront()
 {
   // Check the front sensor
-  return digitalRead(sensor_front) < forward_threshold;
+  return sensor_front < forward_threshold;
 }
