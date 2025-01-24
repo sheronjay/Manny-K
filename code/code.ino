28375bc4392db122f52d0ac43258ef6444090996
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2025-01-24 23:46:31 +0530

    Implement wall following logic with enhanced sensor handling and control flow

--- code/code.ino
+++ code/code.ino
@@ -22,15 +22,12 @@ void setup()
   wifiSetup();
 
   Serial.println("Initializing sensors...");
-  // initializeSensors();
-  // encoderSetup();
-
-  // wifi loop
-  // wifiLoop(NULL);
+  initializeSensors();
+  encoderSetup();
 
   Serial.println("Setup done");
 
-    // Create a FreeRTOS task for the WiFi loop
+  // Create a FreeRTOS task for the WiFi loop
   xTaskCreate(
       wifiLoop,    // Task function
       "WiFi Task", // Task name
@@ -45,8 +42,54 @@ void setup()
 
 void loop()
 {
-  // readThreeSensors();
+  readThreeSensors();
+
+  if (sensor_front < forward_threshold)
+  {
+    delay(1000);
+
+    if (sensor_left < side_threshold && sensor_right < side_threshold)
+    {
+      printSerialAndSend("Turning back");
+      turnBack();
+    }
+    else if (sensor_left < side_threshold)
+    {
+      printSerialAndSend("Turning right");
+      turnRight();
+    }
+    else if (sensor_right < side_threshold)
+    {
+      printSerialAndSend("Turning left");
+      turnLeft();
+    }
+    else
+    {
+      printSerialAndSend("both sides available, turnning left");
+      turnLeft();
+    }
+  }
+  else
+  {
+    if (sensor_left < side_threshold && sensor_right < side_threshold)
+    {
+      dist_to_single_wall_left = sensor_left;
+      dist_to_single_wall_right = sensor_right;
+      wallFollowPidControl(sensor_left, sensor_right);
+    }
+    else if (sensor_left < side_threshold)
+    {
+      leftWallFollowPidControl(sensor_left);
+    }
+    else if (sensor_right < side_threshold)
+    {
+      rightWallFollowPidControl(sensor_right);
+    }
+    else
+    {
+      noWallFollowPidControl();
+    }
+  }
   // algorithmLoop();
-  printSerialAndSend("Looping...");
-  delay(1000);
+  printSerialAndSend(String(sensor_left) + "," + String(sensor_front) + "," + String(sensor_right));
 }
